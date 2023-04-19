4309a6c2855c3c86eb0c53d6c36f72bfe80b0ae9
Harsh Pandey <pandeyharsh704@gmail.com>
2023-04-19 10:29:52 +0530

    Committed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,13 @@
-import { useEffect, useRef } from "react";
 import { ChatBox } from "../components/chat-box";
 
 function Home() {
-  const chatBoxRef = useRef(null);
-
-  useEffect(() => {
-    if (chatBoxRef.current) {
-      chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
-    }
-  }, [chatBoxRef.current?.innerHTML]);
-
   return (
     <div className="wrapper">
       <section>
         <h1>OpenAI ChatGPT</h1>
         <p>This is a project that uses Chatgpt bot to help you with anything</p>
       </section>
-      <section ref={chatBoxRef}>
+      <section>
         <ChatBox />
       </section>
     </div>
